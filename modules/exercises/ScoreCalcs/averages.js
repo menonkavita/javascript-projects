c738453d9a08f52e49457349264ade2ecf113233
K M <menon_kavita@yahoo.com>
2024-07-11 13:05:32 -0500

    Chapter 13 - Modules: Exercises

--- modules/exercises/ScoreCalcs/averages.js
+++ modules/exercises/ScoreCalcs/averages.js
@@ -17,3 +17,9 @@ function averageForTest(testIndex,scores){
 }
 
 //TODO: Export all functions within an object.
+
+module.exports = {
+  averageForStudent: averageForStudent,
+  averageForTest: averageForTest
+};
+
--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,16 @@
+const { randomArrayElement } = require("../exporting-modules/practiceExports");
+
 function randomFromArray(arr){
   //Your code here to select a random element from the array passed to the function.
+
+  let randomNumber = Math.floor(Math.random() * arr.length);
+  return arr[randomNumber];
+
 }
 
+
 //TODO: Export the randomFromArray function.
+
+module.exports = {
+  randomFromArray : randomFromArray
+}
