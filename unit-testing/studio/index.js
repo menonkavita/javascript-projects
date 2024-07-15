30d3ac17f318dd65ccd7ccc8b3449204a8a1900f
K M <menon_kavita@yahoo.com>
2024-07-15 13:43:46 -0500

    Chapter 14: Unit Testing Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,73 @@
 
+
 let launchcode = {
-  
+  organization : "nonprofit",
+  executiveDirector : "Jeff",
+  percentageCoolEmployees : 100,
+  programsOffered : ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput : function(numValue){
+    //1. When passed a number that is ONLY divisible by 2, return 'Launch!'
+    if((numValue % 2 === 0) && (numValue % 3 !== 0) && (numValue % 5 !== 0)){          
+      return `Launch!`
+    }
+
+    // 2. When passed a number that is ONLY divisible by 3, return 'Code!'
+    if((numValue % 3 === 0  && (numValue % 2 !== 0) && (numValue % 5 !== 0))){         
+      return `Code!`
+    }
+
+    // 3. When passed a number that is ONLY divisible by 5, return 'Rocks!'
+    if((numValue % 5 === 0 && (numValue % 2 !== 0) && (numValue % 3 !== 0))){          
+    return `Rocks!`
+    }
+
+    // 4. When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'
+    if((numValue % 2 === 0) && (numValue % 3 === 0) && (numValue % 5 !== 0)){
+      return 'LaunchCode!';
+    }
+
+    // 5. When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'
+    if((numValue % 3 === 0) && (numValue % 5 === 0) && (numValue % 2 !== 0)){
+      return 'Code Rocks!';
+    }
+
+    // 6. When passed a number that is divisible by 2 AND 5, return 'Launch Rocks!'
+    if((numValue % 2 === 0) && (numValue % 5 === 0) && (numValue % 3 !== 0)){
+      return 'Launch Rocks!';
+    }
+
+    // 7. When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'
+    if((numValue % 2 === 0) && (numValue % 3 === 0) && (numValue % 5 === 0)){
+      return 'LaunchCode Rocks!';
+    }
+
+    // 8. When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'
+    if((numValue % 2 !== 0) && (numValue % 3 !== 0) && (numValue % 5 !== 0)){
+      return "Rutabagas! That doesn't work.";
+    }
+  }
 }
+
+console.log("launchcode.organization", launchcode.organization)
+console.log('launchcode["executiveDirector"]', launchcode["executiveDirector"])
+console.log('launchcode["percentageCoolEmployees"]', launchcode["percentageCoolEmployees"])
+console.log("launchcode.programsOffered[0]", launchcode.programsOffered[0])
+console.log("launchcode.programsOffered[1]", launchcode.programsOffered[1])
+console.log("launchcode.programsOffered[2]", launchcode.programsOffered[2])
+console.log("launchcode.programsOffered.length", launchcode.programsOffered.length)
+
+console.log("launchcode.launchOutput(14)", launchcode.launchOutput(14))
+console.log("launchcode.launchOutput(33)", launchcode.launchOutput(33))
+console.log("launchcode.launchOutput(25)", launchcode.launchOutput(25))
+console.log("launchcode.launchOutput(6)", launchcode.launchOutput(6))
+console.log("launchcode.launchOutput(15)", launchcode.launchOutput(15))
+console.log("launchcode.launchOutput(10)", launchcode.launchOutput(10))
+console.log("launchcode.launchOutput(30)", launchcode.launchOutput(30))
+console.log("launchcode.launchOutput(17)", launchcode.launchOutput(17))
+console.log("launchcode.launchOutput(31)", launchcode.launchOutput(31))
+console.log("launchcode.launchOutput(49)", launchcode.launchOutput(49))
+
+
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,106 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+
+  // organization: 1. Inside the describe function in launchcode.test.js, write a test that will check that 
+  //                  the value of organization is "nonprofit". Run your test.
+
+  test("test 1: should have value of organization as 'nonprofit'", () => {
+    let output = launchcode.organization;
+    expect(output).toBe("nonprofit");
+
+  });
+
+
+  // executiveDirector: 1. Write a new test that will check that the value of executiveDirector is "Jeff". Run your test.  
+  test("test 2: should have value of executiveDirector as 'Jeff'", () => {
+    let output = launchcode["executiveDirector"];
+    expect(output).toBe("Jeff");
+  });
+
+
+//percentageCoolEmployees: 1. Write another test that will check that the value of percentageCoolEmployees. Run your test.
+test("test 3: should have value of percentageCoolEmployees", () => {
+  let output = launchcode["percentageCoolEmployees"];
+  //expect(output).not.toBeEmpty();
+  //expect(output).exists();
+  expect(output).toBe(100);
+});
+
+/* programsOffered: 1. Write a fourth test that will check the value of programsOffered. 
+                    You should have four expect statements within your test. Three of them should check that the array 
+                    contains the appropriate values and the final one should check that the array is the appropriate size. 
+                    Before moving on, take a moment either individually or with a classmate, to reflect on why you would 
+                    need these four expect statements. Run your test.
+*/
+
+test("test 4: should check array contains appropriate values and array is appropriate size", () =>{
+  let output = launchcode.programsOffered[0];
+  expect(output).toContain("Web Development");
+
+  output = launchcode.programsOffered[1];
+  expect(output).toContain("Data Analysis");
+
+  output = launchcode.programsOffered[2];
+  expect(output).toContain("Liftoff");
+
+  output = launchcode.programsOffered.length;
+  expect(output).toBe(3);
+});
+
+// launchOutput(): 1. When passed a number that is ONLY divisible by 2, return 'Launch!'
+test("test 5: should return 'Launch' when passed a number divisable ONLY by 2", () => {
+  let output = launchcode.launchOutput(14);
+  expect(output).toBe('Launch!')
+});
+
+/* Write the Next Two Tests: 
+    2. When passed a number that is ONLY divisible by 3, launchOutput() returns 'Code!'
+    3. When passed a number that is ONLY divisible by 5, launchOutput() returns 'Rocks!'
+*/
+
+test("test 6: should return 'Code!' when passed a number divisable ONLY by 3", () => {
+  let output = launchcode.launchOutput(33);
+  expect(output).toBe('Code!');
+});
+
+test("test 7: should return 'Rocks!' when passed a number divisable ONLY by 5", () => {
+  let output = launchcode.launchOutput(25);
+  expect(output).toBe('Rocks!');
+});
+
+// 4. When passed a number that is divisible by  ONLY 2 AND 3, return 'LaunchCode!'
+test("test 8: should return 'LaunchCode!' when passed a number that is divisible by 2 AND 3", () => {
+  let output = launchcode.launchOutput(6);
+  expect(output).toBe('LaunchCode!');
+});
+
+// 5. When passed a number that is divisible by  ONLY 3 AND 5, return 'Code Rocks!'
+test("test 9: should return 'Code Rocks!' when passed a number that is divisible by 3 AND 5", () => {
+  let output = launchcode.launchOutput(15);
+  expect(output).toBe('Code Rocks!');
+});
+
+
+// 6. When passed a number that is divisible by ONLY 2 AND 5, return 'Launch Rocks!'
+test("test 10: should return 'Launch Rocks!' when passed a number that is divisible by 2 AND 5", () => {
+  let output = launchcode.launchOutput(10);
+  expect(output).toBe('Launch Rocks!');
+});
+
+
+// 7. When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'
+test("test 11: should return 'Launch Rocks!' when passed a number that is divisible by 2, 3 AND 5", () => {
+  let output = launchcode.launchOutput(30);
+  expect(output).toBe('LaunchCode Rocks!');
+});
+
+
+// 8. When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'
+test("test 12: should return 'Launch Rocks!' when passed a number that is NOT divisible by 2, 3 AND 5", () => {
+  let output = launchcode.launchOutput(31);
+  expect(output).toBe("Rutabagas! That doesn't work.");
+});
+
+
 });
