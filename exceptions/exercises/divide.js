35ddafebc99901a8e5a67438a5b217616003c583
K M <menon_kavita@yahoo.com>
2024-07-18 12:35:54 -0500

    Chapter 17 Exceptions: Exercises

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,3 +1,6 @@
+
+const input = require('readline-sync')
+
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
 
 // Your function should return the result of numerator / denominator.
@@ -5,3 +8,21 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+function myDivide(numerator, denominator){
+    if (denominator === 0){
+        throw Error("Attempted to divide by zero.");
+    }
+   
+    return numerator/denominator;
+}
+
+
+let num1, num2;
+num1 = Number(input.question("Enter the number to be divided."));
+num2 = Number(input.question("Enter the that you would like to divide it with."))
+
+console.log(myDivide(num1, num2));
+
+
+
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,8 +1,18 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result;
+
+    try{
+        result = lab.runLab(3);
+    }
+    catch{
+
+      result = "Error thrown";
+    }
+   
     console.log(`${lab.student} code worked: ${result === 27}`);
+    //console.log(`${lab.student} code worked: ${result}`);             // Displays value in result variable
   }
 }
 
@@ -22,3 +32,31 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+
+// Incorrectly named lab () in one of the students.
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+console.log("\nProcessing - Students Labs 2")
+gradeLabs(studentLabs2);
+
