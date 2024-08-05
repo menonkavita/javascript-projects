30e4acfc13f7fe0eed042b0b03e80fb200f4b0e0
K M <menon_kavita@yahoo.com>
2024-08-05 16:22:28 -0500

    Chapter 22: DOM and Events - Exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,28 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+
+     missionAbort.addEventListener('mouseover', event =>{
+            //document.getElementById("abortMission").style.backgroundColor="red";
+            missionAbort.style.backgroundColor="red";
+     });
+
+     missionAbort.addEventListener("mouseout", event =>{
+        missionAbort.style.backgroundColor="";
+     });
+
+     missionAbort.addEventListener('click', event =>{
+        if(window.confirm("Are you sure you want to abort the mission?"))
+        {
+            window.alert("Misison aborted! Space Shuttle returning home.");
+            paragraph.innerHTML = "Misison aborted! Space Shuttle is on the way home.";
+        }
+        
+     });
     
 }
 
