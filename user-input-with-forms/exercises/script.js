9ec1228b6928f264e92532c12c4eaea994f01fad
K M <menon_kavita@yahoo.com>
2024-08-08 15:01:18 -0500

    Chapter 25: User Input with Forms getting TypeError

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,28 @@
 //Code Your Solution Below
+
+
+  window.addEventListener("load", function () {
+    let form = document.querySelector("form");
+    console.log("Form loaded");
+
+    form.addEventListener("submit", function (event) {
+      let name = document.querySelector("input[name=testName]");
+      let date = document.querySelector("input[name=testDate]");
+
+      let type = document.querySelector("#rocketType");
+        
+      
+      let number = document.querySelector("input[name=boosterCount]");
+      let rating = document.querySelector("radio[name=windRating]");
+      let servers = document.querySelector("checkbox[name=productionServers]");
+
+      if (name.value === "" || date.value === "" || type.value === "" ||
+        number.value === "" || rating.value === "" || servers.value === "") {
+        alert("Some fields are empty. Please fill all fields.");
+        event.preventDefault();
+      }
+      else {
+        alert("submit clicked: Username is: " + userNameInput.value);
+      }
+    });
+  });
